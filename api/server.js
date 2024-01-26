0d828999e2a61d0f1a9216b3e2218bf3cd0dd583
Viihctor <131902065+Viihctor@users.noreply.github.com>
2024-01-26 01:10:48 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,35 +1,42 @@
 // See https://github.com/typicode/json-server#module
-// ----------------------------------
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const fs = require('fs');
+const path = require('path');
 
 // See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const fs = require('fs')
-const path = require('path')
-
-
 
 // Uncomment to allow write operations
-const filePath = path.join('db.json')
-const data = fs.readFileSync(filePath, "utf-8");
+const filePath = path.join('db.json');
+const data = fs.readFileSync(filePath, 'utf-8');
 const db = JSON.parse(data);
-const router = jsonServer.router(db)
+const router = jsonServer.router(db);
 
 // Comment out to allow write operations
-// const router = jsonServer.router('db.json')
+// const router = jsonServer.router('db.json');
 
-const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
+// Set CORS headers to allow requests from any origin
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+server.use(middlewares);
+
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
+}));
+
+server.use(router);
+
 server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+    console.log('JSON Server is running');
+});
 
 // Export the Server API
-module.exports = server 
+module.exports = server;
