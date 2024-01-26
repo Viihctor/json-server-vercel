ed57b15fe32b01d5090f761e5adf3a1914a030b4
Viihctor <131902065+Viihctor@users.noreply.github.com>
2024-01-25 21:42:02 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,45 +1,22 @@
 // See https://github.com/typicode/json-server#module
-
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-
-server.use(middlewares);
-
-// Middleware para habilitar o CORS
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
-
-server.use(router);
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
-});
-
-
 const jsonServer = require('json-server')
 
 const server = jsonServer.create()
 
 // Uncomment to allow write operations
-const fs = require('fs')
-const path = require('path')
-const filePath = path.join('db.json')
-const data = fs.readFileSync(filePath, "utf-8");
-const db = JSON.parse(data);
-const router = jsonServer.router(db)
+// const fs = require('fs')
+// const path = require('path')
+// const filePath = path.join('db.json')
+// const data = fs.readFileSync(filePath, "utf-8");
+// const db = JSON.parse(data);
+// const router = jsonServer.router(db)
 
 // Comment out to allow write operations
-//const router = jsonServer.router('db.json')
+const router = jsonServer.router('db.json')
 
-//const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults()
 
-//server.use(middlewares)
+server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
