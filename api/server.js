eff401e0a6a0cc8fb5ebd923fe0d587b468cc6cc
Viihctor <131902065+Viihctor@users.noreply.github.com>
2024-01-25 21:49:55 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,7 +1,4 @@
 // See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-
-const server = jsonServer.create()
 
 // Uncomment to allow write operations
 // const fs = require('fs')
@@ -29,3 +26,37 @@ server.listen(3000, () => {
 
 // Export the Server API
 module.exports = server
+// ----------------------------------
+
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const fs = require('fs')
+const path = require('path')
+
+
+
+// Uncomment to allow write operations
+const filePath = path.join('db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
+
+// Comment out to allow write operations
+// const router = jsonServer.router('db.json')
+
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
+
+// Export the Server API
+module.exports = server 
